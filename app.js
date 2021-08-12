47976b03e9e25b77d77d3370e44fab9555923cc0
sautmanurung1 <68042498+sautmanurung1@users.noreply.github.com>
2021-08-12 17:29:53 +0000

    deploying

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 app.use('/static',express.static(path.join(__dirname,'static')));
+app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'static','index.html'));
+    res.render(index);
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
