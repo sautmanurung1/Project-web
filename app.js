7761783a58c4bc79993ba45837e9cfaeaf701f7f
sautmanurung1 <68042498+sautmanurung1@users.noreply.github.com>
2021-08-13 04:52:42 +0000

    publish

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use('/static',express.static(path.join(__dirname,'static')));
 app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
-    res.render(index);
+    res.render('index');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
